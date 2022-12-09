7f43673611304bb3eb4c5c1b2c9106b1815a6ad8
qle107 <118285772+qle107@users.noreply.github.com>
2022-12-09 09:53:17 +0100

    Add files via upload

--- Classes/Spells/Spell.php
+++ Classes/Spells/Spell.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace App\Classes\Spells;
+use App\Classes\Characters\Character;
 abstract class Spell{
 
     function __construct(
         public string $spellName,
         public string $description,
         public int $value,
-        
+        public int $manacost,
     )
     {
         
     }
+    abstract function castspell(Character $character,int $value,int $manacost);
 }
