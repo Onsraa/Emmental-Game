129e307864a4000945e008639efc472552fca034
Teddy TRUONG <truongteddy306@icloud.com>
2022-12-20 18:03:21 +0100

    update spell properties

--- Classes/Spells/Spell.php
+++ Classes/Spells/Spell.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Classes\Spells;
-class Spell {
 
+class Spell
+{
 
+    function __construct(
+        public string $spellName,
+        public int $cost,
+        public int $value
+    ) {
+    }
 }
-
-?>
