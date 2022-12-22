a6244889bfcd6c9ce5fc94307ae89cf2c351c50b
Milo <lhilderal@myges.fr>
2022-12-22 09:32:17 +0100

    new Specialisation availbale : Flower Fairy

---
+++ Classes/Specialisations/FlowerFairy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Classes\Specialisations;
+
+use App\Classes\Characters\Character;
+
+class FlowerFairy extends Character
+{
+
+    public function __construct()
+    {
+        parent::__construct(
+            className: "Flower Fairy",       // basically the specialization name of the character
+            element: "plant",              // the element which will define who he is weak against
+            health: 900,                 // total fixed health points
+            mana: 400,
+            physicalStrength: 50,        // basic stats without weapons and stuffs
+            magicalStrength: 250,
+            physicalDefense: 100,
+            magicalDefense: 200,
+        );
+    }
+}
--- index.php
+++ index.php
@@ -6,8 +6,10 @@ require_once("./Functions/fight.php");
 
 use App\Classes\Specialisations\Chaman;
 use App\Classes\Specialisations\Draconist;
+use App\Classes\Specialisations\FlowerFairy;
 
 $player1 = new Draconist();
 $player2 = new Chaman();
+$player3 = new FlowerFairy();
 
-fight($player1, $player2);
+fight($player1, $player3);
